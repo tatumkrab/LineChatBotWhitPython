8863c27fa1f86ab94f35258ed228039a9aeb5af0
tatumkrab <49138630+tatumkrab@users.noreply.github.com>
2019-10-02 14:37:19 +0700

    first commit

---
+++ Project/Config.py
@@ -0,0 +1,11 @@
+
+
+
+
+Channel_secret = "c7d0a84016ebe91e71cc69585826a9e8"
+
+
+Channel_access_token = "7lXVSefclnpwcVQ9uQqZ2tUOJGtLGdygL5/BGYBppDz0zGo4dvdObKRvloXJKxU/82xNNc+YOKHa/+NZFYdBC1HlHcZjNswGrNYAwwCKBU1xEyjG4qohrlSRXbK7Dt4octjusu4O7HRP71uEoaPtcAdB04t89/1O/w1cDnyilFU="
+
+
+Basic_ID = "@284ccdsf"
